1668818cb82286796d816fbd2371dbd903d38f95
Nicolas Gutierrez <yefry-nicolas123@hotmail.com>
2024-03-03 10:19:34 -0500

    logs added

--- loadBalancer.js
+++ loadBalancer.js
@@ -9,16 +9,32 @@ require('dotenv').config();
 const express = require('express');
 const request = require('request');
 const cors = require('cors');
+//const moment = require('moment');
 
 const app = express();
 
 app.use(cors());
 
+//app.use(express.json());
+
+
+app.use((req, res, next) => {
+    const now = new Date();
+    const formattedDate = `${now.getDate()}/${now.getMonth()+1}/${now.getFullYear()}:${now.getHours()}/${now.getMinutes()}/${now.getSeconds()}`;
+    const clientIp = req.ip.split(':').pop();
+    //console.log(`${clientIp} ${formattedDate} ${req.method} ${req.originalUrl}`);
+    console.log(`[${clientIp}] [${formattedDate}]`);
+    //[${JSON.stringify(req.body)}] [${req.method}] [${req.originalUrl}]
+
+    next();
+});
+
+
 const SERVER_IP_1 = process.env.SERVER_IP_1;
 const SERVER_IP_2 = process.env.SERVER_IP_2;
 const SERVER_IP_3 = process.env.SERVER_IP_3;
 
-console.log('SERVER_IP_1:', SERVER_IP_1);
+//console.log('SERVER_IP_1:', SERVER_IP_1);
 
 const servers = [
     SERVER_IP_1,
@@ -43,15 +59,18 @@ const handler = (req, res) => {
 
     const _req = request({url: serverUrl + req.url});
     console.log('redirecting to server', serverUrl);
-    console.log('client IP:', clientIp);
+    //console.log('client IP:', clientIp);
 
     _req.on('error', function(err) {
-        console.log(`Error: El servidor en la dirección ${serverUrl} no está disponible.`);
+        console.log(`Error: Server refused connection ${serverUrl} is not available.`);
+        console.log('Error time:', new Date().toISOString());
+        console.log('Error payload:', err);
         delete ipServerMap[clientIp];
         handler(req, res);
     });
 
     req.pipe(_req).pipe(res);
+    //next();
 }
 
 const server = app.get('*', handler).post('*', handler);
